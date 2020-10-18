95506a0dc24cb72d6493355fc668e73f7372d25b
TransformsGit <64213593+TransformsGit@users.noreply.github.com>
2020-10-18 14:01:54 +0300

    Add files via upload

---
+++ 2copy_dir_build_win.bat
@@ -0,0 +1,5 @@
+net use \\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9 /u:saguaroprint.ro\cmorariu New42day1 /p:yes
+ 
+
+copy "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\*.*" "d:\Builds\"
+echo "\\172.27.112.113\sw_transforms\public\ITM\Kit\ITM_3.9.0\ITM_3.9.0-win\cycle9\*.*" "d:\Builds\"
